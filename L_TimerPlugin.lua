f2c70b4766eb479d6ddbc7d60f9cd3843966590d
Frede175 <fsr1998@hotmail.com>
2016-08-04 02:16:08 +0200

    sync

--- L_TimerPlugin.lua
+++ L_TimerPlugin.lua
@@ -1,9 +1,26 @@
+json = require('json')
 
 
+local tpConfig
 
+ -- Sets the config from the json
+function tpSetConfig(data)
+	tpConfig = json.decode(data)
+end
+
+
+ -- Make sure the config is loaded
+function GetTpConfig()
+	if (tpConfig == nil) then
+		tpConfig = tpSetConfig(tpData)
+	end
+end
 
 
  -- Function called on device startup
-function lrStartup()
+function tpStartup()
+	GetTpConfig()
+
 	
+
 end
---
+++ L_TimerPlugin_Configuration.lua
@@ -0,0 +1,26 @@
+tpData = [[
+[
+	{
+		"name": "Restroom",
+		"sensorsID": {
+			"1" : ""
+		},
+		"switchsID" : {
+			"1": ""	
+		},
+		"timeOn" : "120"
+	},
+	{
+		"name": "Water Pump",
+		"sensorsID": {
+			"1" : "",
+			"2" : ""
+		}, 
+		"switchsID" : {
+			"1": ""	
+		},
+		"timeOn" : "120"	
+	}
+
+]
+]]
